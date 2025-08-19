8f36eca2cea5396c9afeb9c1451157c134735dd0
Jerrytys <jerrytys03@gmail.com>
2025-08-19 13:10:18 +0800

    Add greeting

--- src/main/java/Betty.java
+++ src/main/java/Betty.java
@@ -1,6 +1,14 @@
 public class Betty {
-    
-    public static void main(String[] args) {
 
+    public static void greeting() {
+        System.out.println("-----------------------------------");
+        System.out.println("Hello! I'm Betty");
+        System.out.println("What can I do for you?");
+        System.out.println("-----------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("-----------------------------------");
+    }
+    public static void main(String[] args) {
+        greeting();
     }
 }

149c7f6446919f2028199f5cfba785af6eee9d6e
Jerrytys <jerrytys03@gmail.com>
2025-08-20 12:26:43 +0800

    Added support for echo

--- src/main/java/Betty.java
+++ src/main/java/Betty.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Betty {
 
     public static void greeting() {
@@ -5,10 +7,29 @@ public class Betty {
         System.out.println("Hello! I'm Betty");
         System.out.println("What can I do for you?");
         System.out.println("-----------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("-----------------------------------");
     }
     public static void main(String[] args) {
+        // Greeting by chatbot
         greeting();
+
+        // Create scanner to take input by user
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            // Listen for input by user
+            String input = scanner.nextLine();
+            // Break when condition "bye" is met
+            if (input.equals("bye")) {
+                System.out.println("-----------------------------------");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("-----------------------------------");
+                break;
+            } else {
+                System.out.println("-----------------------------------");
+                System.out.println(input);
+                System.out.println("-----------------------------------");
+            }
+        }
+
+
     }
 }

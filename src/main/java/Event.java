8a0fded29c2059aebc0ad9b07d0d0d756a22f77e
Jerrytys <jerrytys03@gmail.com>
2025-08-26 17:07:37 +0800

    Edit Event to include LocalDate

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,23 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String from;
-    protected String to;
+    protected LocalDate from;
+    protected LocalDate to;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+
+        this.from = LocalDate.parse(from);
+        this.to = LocalDate.parse(to);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+        // Format time to pattern MMM dd yyyy
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MMM dd yyyy");
+        return "[E]" + super.toString() + " (from: " + this.from.format(dateFormat) +
+                " to: " + this.to.format(dateFormat) + ")";
     }
 }

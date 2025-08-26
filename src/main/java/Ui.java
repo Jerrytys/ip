84341d1028e413c7d5bad5685e7bf5449fa39562
Jerrytys <jerrytys03@gmail.com>
2025-08-26 18:58:59 +0800

    Add more features for Ui class

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -14,19 +14,34 @@ public class Ui {
     public void greeting() {
         printBox("Hello! I'm Betty\nWhat can I do for you?");
     }
+    // Function to print goodbye message
+    public void goodbye() {
+        printBox("Bye. Hope to see you again soon!");
+    }
 
     // Displays the list of tasks
-    public void displayList(File TaskFile) {
-        int count = 1;
-        StringBuilder message = new StringBuilder();
-        try {
-            Scanner scanner = new Scanner(TaskFile);
-            while (scanner.hasNextLine()) {
-                message.append(scanner.nextLine() + "\n");
-            }
-        } catch (FileNotFoundException e) {
-            System.err.println("File not found: " + e.getMessage());
-        }
-        printBox(String.valueOf(message));
+    public void displayList(TaskList taskList) {
+        printBox(taskList.toString());
+    }
+    // Add task
+    public void addTask(TaskList taskList) {
+        printBox("Got it. I've added this task: \n" +
+                "  " + taskList.get(taskList.size() - 1 ).toString() +
+                "\nNow you have " + taskList.size() + " tasks in the list.");
+    }
+    public void deleteTask(String message, Task task, TaskList taskList) {
+        StringBuilder deleteMessage = new StringBuilder();
+        deleteMessage.append("Noted, I've removed this task:\n")
+                        .append(task.toString())
+                            .append("\nNow you have ")
+                                .append(taskList.size()).append(" tasks in the list.");
+        printBox(String.valueOf(deleteMessage));
+    }
+    // Message for mark done and undone
+    public void markDone(TaskList taskList, int number) {
+        printBox("Nice! I've marked this task as done:\n" + taskList.get(number).toString());
+    }
+    public void markUndone(TaskList taskList, int number) {
+        printBox("OK, I've marked this task as not done yet:\n" + taskList.get(number).toString());
     }
 }

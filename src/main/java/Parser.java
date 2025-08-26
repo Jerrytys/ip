8783b4ac99bce531cb8175759adb5e935897e688
Jerrytys <jerrytys03@gmail.com>
2025-08-26 17:43:37 +0800

    Create Parser class with parse function for task

---
+++ src/main/java/Parser.java
@@ -0,0 +1,35 @@
+public class Parser {
+
+    public static Task parse(String taskString) {
+        // Create parsing for different cases of task
+        String[] arguments = taskString.split(" \\| ", 5);
+        String type = "";
+        String completed = "0";
+        String description = "";
+        String deadline;
+        String to;
+        String from;
+
+        switch (type) {
+            case "T":
+                completed = arguments[1];
+                description = arguments[2];
+                return new Todo(description);
+            case "D":
+                completed = arguments[1];
+                description = arguments[2];
+                deadline = arguments[3];
+                return new Deadline(description, deadline);
+            case "E":
+                completed = arguments[1];
+                description = arguments[2];
+                from = arguments[3];
+                to = arguments[4];
+                return new Event(description, from, to);
+            default:
+               // TODO: THROW ERROR  
+        }
+
+
+    }
+}

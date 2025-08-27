c729c9cb2aebe7d5b43587e4709971878f6ec9d7
Jerrytys <jerrytys03@gmail.com>
2025-08-27 12:56:46 +0800

    Add dummy test

---
+++ src/test/java/betty/BettyTest.java
@@ -0,0 +1,18 @@
+package betty;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BettyTest {
+
+    @Test
+    public void dummyTest() {
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void anotherDummyTest() {
+        assertEquals(4, 4);
+    }
+}

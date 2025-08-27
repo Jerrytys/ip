f1662c8e2d0c22ae24efabd2fde93d42b6db38d9
Jerrytys <jerrytys03@gmail.com>
2025-08-27 12:57:46 +0800

    Add non trivial test methods to class

--- src/test/java/betty/task/TaskListTest.java
+++ src/test/java/betty/task/TaskListTest.java
@@ -1,4 +1,22 @@
 package betty.task;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TaskListTest {
+
+    @Test
+    public void createAllThreeTaskSuccess() throws Exception {
+        TaskList tasks = new TaskList();
+        tasks.addTodo("read book");
+        tasks.addDeadline("return book /by 2025-09-01");
+        tasks.addEvent("go to library /from 2025-09-01 /to 2025-09-01");
+
+        assertEquals(3, tasks.size());
+        assertTrue(tasks.get(0) instanceof Todo);
+        assertTrue(tasks.get(1) instanceof Deadline);
+        assertTrue(tasks.get(2) instanceof Event);
+    }
 }
--- src/test/java/betty/task/TodoTest.java
+++ src/test/java/betty/task/TodoTest.java
@@ -1,4 +1,24 @@
 package betty.task;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TodoTest {
+
+    @Test
+    public void todo_toString_correctFormat() {
+        Task task = new Todo("read book");
+
+        assertEquals("T |   | read book", task.toString());
+    }
+
+    @Test
+    public void todo_toString_markDone() {
+        Task todo = new Todo("read book");
+        todo.markAsDone();
+
+        assertEquals("T | X | read book", todo.toString());
+    }
+
 }

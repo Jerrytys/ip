23bae03639aabb607ac9d7d4c20c06f1447406c1
Jerrytys <jerrytys03@gmail.com>
2025-08-27 18:19:59 +0800

    Create Command class in package command

---
+++ src/main/java/betty/command/AddTodoCommand.java
@@ -0,0 +1,20 @@
+package betty.command;
+
+import betty.exception.BettyException;
+import betty.storage.Storage;
+import betty.task.TaskList;
+import betty.ui.Ui;
+
+public class AddCommand extends Command {
+
+
+    @Override
+    public void execute(TaskList taskList, Ui ui, Storage storage) throws BettyException {
+
+    }
+
+    @Override
+    public boolean isExit() {
+
+    }
+}

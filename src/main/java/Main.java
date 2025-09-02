09b75b582fcb7543863ad94f0a5b14ad927e8bb5
Jerrytys <jerrytys03@gmail.com>
2025-09-02 18:15:26 +0800

    Refactor Betty to use FXML

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import betty.Betty;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -10,89 +11,28 @@ import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 
 public class Main extends Application {
 
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    // Loads User profile picture
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
     private Betty betty = new Betty();
 
     @Override
     public void start(Stage stage) {
         //Setting up required components
-
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        // Create a dialog box that echoes user input using send button
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        //Formatting the window to look as expected
-
-        stage.setTitle("Betty");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        //Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-        //More code to be added here later
-    }
-
-    private void handleUserInput() {
-        String userText = userInput.getText();
-        String bettyText = betty.getResponse(userText);
-        DialogBox userBox = DialogBox.getUserDialog(userText, userImage);
-        DialogBox bettyBox = DialogBox.getBettyDialog(bettyText, userImage);
-        // Add dialog box into dialog container
-        dialogContainer.getChildren().addAll(userBox, bettyBox);
-        // Clear user input after creating dialog box
-        userInput.clear();
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setTitle("Betty");
+            stage.setScene(scene);
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
+            fxmlLoader.<MainWindow>getController().setBetty(betty); // inject the Betty instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,2 +1,52 @@
-public class MainWindow {
+import betty.Betty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Betty betty;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image bettyImage = new Image(this.getClass().getResourceAsStream("/images/Dauser.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /** Injects the Duke instance */
+    public void setBetty(Betty b) {
+        betty = b;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = betty.getResponse(input);
+        DialogBox userBox = DialogBox.getUserDialog(input, userImage);
+        DialogBox bettyBox = DialogBox.getBettyDialog(response, userImage);
+        // Add dialog box into dialog container
+        dialogContainer.getChildren().addAll(userBox, bettyBox);
+        // Clear user input after creating dialog box
+        userInput.clear();
+    }
 }

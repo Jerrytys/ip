3b2df9870e6adced8a040f1c34fd0a8097cf5804
Jerrytys <jerrytys03@gmail.com>
2025-09-10 00:58:18 +0800

    Update imports

--- src/main/java/betty/parser/Parser.java
+++ src/main/java/betty/parser/Parser.java
@@ -6,22 +6,9 @@ import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.List;
 
-import betty.command.AddDeadlineCommand;
-import betty.command.AddEventCommand;
-import betty.command.AddTodoCommand;
-import betty.command.ByeCommand;
-import betty.command.Command;
-import betty.command.DeleteCommand;
-import betty.command.FindCommand;
-import betty.command.ListCommand;
-import betty.command.MarkTaskCommand;
-import betty.command.UnmarkTaskCommand;
+import betty.command.*;
 import betty.exception.BettyException;
-import betty.task.Deadline;
-import betty.task.Event;
-import betty.task.Task;
-import betty.task.Todo;
-import betty.ui.Ui;
+import betty.task.*;
 
 /**
  * Represents a parser class that can parse strings into other useful objects

06e50b0ea88504ae20bb868c96c14fd8fd2e7295
Jerrytys <jerrytys03@gmail.com>
2025-09-10 00:45:59 +0800

    Add sortByPriority method

--- src/main/java/betty/task/TaskList.java
+++ src/main/java/betty/task/TaskList.java
@@ -1,6 +1,7 @@
 package betty.task;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -117,6 +118,7 @@ public class TaskList {
      */
     @Override
     public String toString() {
+        sortByPriority();
         StringBuilder sb = new StringBuilder();
         int counter = 1;
         for (Task t : this.taskList) {
@@ -132,6 +134,7 @@ public class TaskList {
      * @return a formatted string with each individual task for storage
      */
     public String toSaveString() {
+        sortByPriority();
         StringBuilder sb = new StringBuilder();
         for (Task t : this.taskList) {
             sb.append(t.toSaveString()).append("\n");
@@ -148,5 +151,9 @@ public class TaskList {
         Stream<Task> streamList = this.taskList.stream();
         Stream<Task> filteredList = streamList.filter(t -> t.getDescription().contains(filter));
         return new TaskList(filteredList.toList());
+    }
+    public void sortByPriority() {
+        Comparator<Task> comparator = Comparator.comparingInt(t -> t.getPriority().getRank());
+        this.taskList.sort(comparator);
     }
 }
